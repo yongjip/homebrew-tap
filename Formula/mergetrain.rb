fc7ecbfc9568b0bfd1c98f62ef0ddcd74a88545d
yongjip <13984989+yongjip@users.noreply.github.com>
2026-09-02 01:49:19 +0900

    Add PyYAML resource for mergetrain

--- Formula/mergetrain.rb
+++ Formula/mergetrain.rb
@@ -12,6 +12,11 @@ class Mergetrain < Formula
   depends_on "libyaml"
   depends_on "python@3.14"
 
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
+    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+  end
+
   def install
     virtualenv_install_with_resources
   end
